7ff8d0c861d611f6c230e3db15daee45e4d9ff23
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-14 16:28:25 +0000

    added bubble_sort function prototype

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
