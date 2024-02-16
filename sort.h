8a604473239076c7dabdf042806046fa9eb7801e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-16 14:42:16 +0000

    added function prototype for cocktail_sort algorithm

--- sort.h
+++ sort.h
@@ -24,6 +24,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
