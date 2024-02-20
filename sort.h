aebbbebefa99ace634b69212c5bb8c8c46606c16
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-20 14:03:21 +0000

    removed unused data structure

--- sort.h
+++ sort.h
@@ -43,20 +43,5 @@ void bitonic_seq(int *array, size_t size, size_t seq, char flow);
 void _quicksort(int *array, int size, int left, int right, int part_idx);
 int partition(int *array, int size, int left, int right);
 
-/* Structure for the Radix sort algorithm */
-
-/**
- * struct digit_node - Structure to represent a node in a linked list of
- * digits.
- *
- * @value: Value of the digit.
- * @next: Pointer to the next digit node.
- */
-typedef struct digit_node
-{
-	int value;
-	struct digit_node *next;
-} digit_node;
-
 
 #endif /* SORT_H */
